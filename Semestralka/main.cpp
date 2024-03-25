6fd8cbed62bdcfa3a75987c7e8112545c99b9d82
Nikolas Novak <nklasakk@gmail.com>
2024-03-25 23:28:49 +0100

    used algorithm with matchaddres

--- Semestralka/main.cpp
+++ Semestralka/main.cpp
@@ -1,6 +1,7 @@
 //#include <iostream>
 #include <vector>
 #include "RTParser.h"
+#include "Algorithm.h"
 
 int main(){
     RTParser rtparser("RT.csv");
@@ -12,13 +13,40 @@ int main(){
 		if (entry == nullptr) {
 			continue;
 		}
-		std::cout << i << " ";
-		entry->print();
+		/*std::cout << i << " ";
+		entry->print();*/
 		rt.push_back(entry);
 		++i;
 	}
 
-	for (const auto& entry : rt) {
-		delete entry;
+	std::vector<TableEntry> matches;
+
+	IPAddress* matchAddress = new IPAddress("1.0.0.1");
+	auto matchWithAddressPredicate = [&matchAddress](TableEntry otherEntry) {
+		int maskTmp = otherEntry.getAddress()->getMaskLength();
+		for (size_t i = 0; i < 4; i++) {
+			if (otherEntry.getAddress()->getAddressOctet(i) != (matchAddress->getAddressOctet(i) & IPAddress::createMaskNumber(maskTmp > 8 ? 8 : maskTmp))) {
+				return false;
+			}
+			if (maskTmp >= 8) {
+				maskTmp -= 8;
+			}
+		}
+		return true;
+		};
+
+	Algorithm::matchAddressBy(rt.begin(), rt.end(), matches, 
+		[](std::vector<TableEntry>& structure, TableEntry& entry) {
+			structure.push_back(entry);
+		}, matchWithAddressPredicate);
+
+	//std::printf("Found matches\n");
+	std::cout << "Found matches\n";
+	for (auto entry : matches) {
+		entry.print();
 	}
+
+	/*for (const auto& entry : rt) {
+		delete entry;
+	}*/
 }
