a7cf7dd00ac27db851a87d539d64a9608ecc73c4
Nikolas Novak <nklasakk@gmail.com>
2024-03-25 23:27:11 +0100

    added algorithm

--- Semestralka/Algorithm.h
+++ Semestralka/Algorithm.h
@@ -1,4 +1,15 @@
 #pragma once
-class IPMatcher {
+class Algorithm {
+public:
+	template <class Iterator, typename Predicate, class MatchesStructure, typename InsertMatch>
+	static void matchAddressBy(Iterator begin, Iterator end, MatchesStructure& matches, InsertMatch inserter, Predicate predicate) {
+		while (begin != end) {
+			auto item = begin;
+			if (predicate(**item)) {
+				inserter(matches, **item);
+			}
+			++begin;
+		}
+	}
 };
 
