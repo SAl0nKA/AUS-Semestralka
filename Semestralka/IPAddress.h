b02d40a57b0dea448d3f6e7d3f9832d0d932dead
Nikolas Novak <nklasakk@gmail.com>
2024-03-25 23:27:55 +0100

    changed unsigned short to uint8, fixed parsing, added getters

--- Semestralka/IPAddress.h
+++ Semestralka/IPAddress.h
@@ -6,9 +6,9 @@
 
 class IPAddress {
 private:
-	unsigned short address[4];
-	unsigned short mask[4];
-    int maskLength;
+    std::uint8_t address_[4];
+	//unsigned short mask_[4];
+    int maskLength_ = 0;
 
 public:
     IPAddress(std::string fullAddressText) {
@@ -28,17 +28,58 @@ public:
 		while (std::getline(IP, part, '.')) {
             //unsigned long result = std::stoul(str, &pos);
             //todo change to stoi
-			address[i] = static_cast<unsigned short>(atoi(part.c_str()));
+            address_[i] = static_cast<std::uint8_t>(atoi(part.c_str()));
             ++i;
 		}
-        maskLength = std::stoi(IPAndMask[1]);
+        
+        maskLength_ = std::stoi(IPAndMask[1] == "" ? "0" : IPAndMask[1]);
 	}
+
+    ~IPAddress() {
+
+    }
+
+    int getMaskLength() {
+        return maskLength_;
+    }
+
+    std::uint8_t getAddressOctet(int index) {
+        if (index < 0 || index > 3) {
+            return -1;
+        }
+        return address_[index];
+    }
 
     std::string string() {
         char buffer[20]; 
-        sprintf_s(buffer, "%hu.%hu.%hu.%hu/%d", address[0], address[1], address[2], address[3], maskLength);
+        sprintf_s(buffer, "%hu.%hu.%hu.%hu/%d", address_[0], address_[1], address_[2], address_[3], maskLength_);
         std::string str(buffer);
         return str;
+    }
+
+    //static bool matchWithAddress(IPAddress matchAddress,IPAddress otherAddress) {
+    //    /*int notFullOctet = matchAddress.maskLength_ % 8;
+    //    int fullOctets = (matchAddress.maskLength_ - notFullOctet) / 8;*/
+    //    int maskTmp = matchAddress.maskLength_;
+    //    for (size_t i = 0; i < 4; i++) {
+    //        if ((otherAddress.address_[i] & createMaskNumber(maskTmp > 8 ? 8 : maskTmp)) != matchAddress.address_[i]) {
+    //            return false;
+    //        }
+    //        maskTmp -= 8;
+    //    }
+    //    return true;
+    //}
+
+//private:
+    static std::uint8_t createMaskNumber(int bits) {
+        std::uint8_t maskNumber = 0;
+        std::uint8_t adder = 128;
+        for (int i = 0; i < bits; i++) {
+            maskNumber += adder;
+            adder /= 2;
+        }
+        //std::cout << "Mask number: " << maskNumber << std::endl;
+        return maskNumber;
     }
 };
 
