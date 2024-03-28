95dc6f477419cc1226e5a45346d7412d5519f3d8
Nikolas Novak <nklasakk@gmail.com>
2024-03-28 21:31:16 +0100

    removed matches structure

--- Semestralka/Algorithm.h
+++ Semestralka/Algorithm.h
@@ -1,12 +1,12 @@
 #pragma once
 class Algorithm {
 public:
-	template <class Iterator, typename Predicate, class MatchesStructure, typename InsertMatch>
-	static void matchAddressBy(Iterator begin, Iterator end, MatchesStructure& matches, InsertMatch inserter, Predicate predicate) {
+	template <class Iterator, typename Predicate, typename InsertMatch>
+	static void matchAddressBy(Iterator begin, Iterator end, InsertMatch inserter, Predicate predicate) {
 		while (begin != end) {
 			auto item = begin;
-			if (predicate(**item)) {
-				inserter(matches, **item);
+			if (predicate(*item)) {
+				inserter(*item);
 			}
 			++begin;
 		}
